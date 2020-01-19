a585cd4deb1ff0778f3fba1354526f1c425842cb
John Joseph <atiquajohn@gmail.com>
2020-01-19 21:44:45 +0700

    change into application

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,10 +1,11 @@
 defmodule Dictionary.WordList do
+  @me __MODULE__
   def start_link() do
-    Agent.start_link(&word_list/0)
+    Agent.start_link(&word_list/0, name: @me)
   end
 
-  def random_word(agent) do
-    agent.get(agent, &Enum.random/1)
+  def random_word() do
+    Agent.get(@me, &Enum.random/1)
   end
 
   def word_list() do
